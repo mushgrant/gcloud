b75066836358845c4d9ac56e855e1208c4c2712a
Marshall Grant <mushgrant@yahoo.com>
2018-03-25 08:12:18 -0400

    Update config.sh

--- data/config.sh
+++ data/config.sh
@@ -5,8 +5,8 @@ export LOCAL_EVAL_DATA=./data/test.csv
 export LOCAL_MODEL_DIR=./output
 
 export REMOTE_MODEL_NAME=classifier_sample
-export REMOTE_BUCKET_NAME=vmware_final
-export REMOTE_TRAIN_DATA=gs://$REMOTE_BUCKET_NAME/data/training.csv
+export REMOTE_BUCKET_NAME=toydatasets
+export REMOTE_TRAIN_DATA=gs://$REMOTE_BUCKET_NAME/data/train.csv
 export REMOTE_EVAL_DATA=gs://$REMOTE_BUCKET_NAME/data/test.csv
 export REMOTE_OUTPUT_PATH=gs://$REMOTE_BUCKET_NAME/output
 export REMOTE_REGION=us-central1
